ff5a8585c76292be30cb9e4712cc5957dc852cb7
Rama Faisal <ramafaisal23@gmail.com>
2024-08-20 18:16:19 +0700

    fillament fix

--- resources/js/Pages/Informasi/BeritaDetail.jsx
+++ resources/js/Pages/Informasi/BeritaDetail.jsx
@@ -10,9 +10,19 @@ const BeritaDetail = ({ article }) => {
             <Navbar />
             <div className="container mx-auto p-4 mt-20">
                 <h1 className="text-4xl font-bold mb-4">{article.title}</h1>
-                <p className="text-gray-600 mb-4">By {article.author || "Admin"} | {new Date(article.created_at).toLocaleDateString()}</p>
-                <img src={article.image} alt={article.title} className="w-full h-full object-cover mb-4" />
-                <div className="text-lg" dangerouslySetInnerHTML={{ __html: article.content }} />
+                <p className="text-gray-600 mb-4">
+                    By {article.author || "Admin"} |{" "}
+                    {new Date(article.created_at).toLocaleDateString()}
+                </p>
+                <img
+                    src={article.image}
+                    alt={article.title}
+                    className="h-20 object-cover mb-4"
+                />
+                <div
+                    className="text-lg"
+                    dangerouslySetInnerHTML={{ __html: article.content }}
+                />
             </div>
             <Footer />
         </>
