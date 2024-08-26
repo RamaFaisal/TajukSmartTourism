0b5d6125771d350b238d1bb2a47d6b8efcfa7b68
Rama Faisal <ramafaisal23@gmail.com>
2024-08-26 17:24:24 +0700

    icon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,9 +4,11 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    
     <title inertia>{{ config('app.name') }}</title>
 
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
